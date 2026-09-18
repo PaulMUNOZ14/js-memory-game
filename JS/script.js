b3e167e82e8aad67c3ad6aa8ae8e15eb84df98ad
MUNOZ Paul <paul.munoz@etu.unicaen.fr>
2026-09-18 09:24:50 +0200

    feat: core array data logic and fisher-yates shuffle functional

--- JS/script.js
+++ JS/script.js
@@ -10,5 +10,11 @@ for (let i = 0; i < 8; i++){
 
 let cards = [...images, ...images];
 
-console.log(images);
-console.log(cards);
+function shuffle(array){
+    for (let i = array.length - 1; i > 0; i--){
+        let j = Math.floor(Math.random() * i)
+        let temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+    }
+}
